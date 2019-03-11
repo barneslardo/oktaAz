828b1e45bca7a12e62b5a0d5373fbed69281bc36
barneslardo <barneslardo@gmail.com>
2019-03-11 06:52:34 -0700

    test2

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express"),
   app = express();
 
-// This is a comment to test flow
+// Test
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log("Server is running on " + PORT));
